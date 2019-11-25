f942efb4e5f752b11d9891ba8141981d6debb6e2
Ferveloper <fermervil@gmail.com>
2019-11-25 12:55:23 +0100

    Edit API routes

---
+++ routes/api-v1/api.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const router = express.Router();
+
+const Trip = require('../../models/Trip');
+const Expense = require('../../models/Expense');
+
+// Add a new trip
+router.post('/trip', async (req, res, next) => {
+
+  try {
+
+    const trip = new Trip({ name: req.body.name });
+    const savedTrip = await trip.save()
+    res.status(200).json({
+      success: true,
+      result: savedTrip
+    });
+
+  } catch (err) {
+
+    res.status(404).json({
+      success: false,
+      result: err.errors.name.message
+    });
+    // next(err);
+    // return;
+
+  }
+
+})
+
+// List all trips
+router.get('/trips', async (req, res, next) => {
+
+  try {
+
+    const trips = await Trip.find();
+
+    res.status(200).json({
+      success: true,
+      results: trips
+    });
+
+  } catch(err) {
+
+    next(err);
+    return;
+
+  }
+
+})
+
+// Add a new expense
+router.post('/expense', async (req, res, next) => {
+
+  try {
+
+    const data = {};
+    trip = await Trip.findOne({ name: req.body.trip });
+    data.tripId = tripId._id;
+    data.date = req.body.date,
+    data.amount = req.body.amount,
+    data.category = req.body.category,
+    data.description = req.body.description
+
+    const expense = new Expense(data);
+    const savedExpense = await expense.save()
+    res.status(200).json({
+      success: true,
+      result: savedExpense
+    });
+
+  } catch (err) {
+
+    next(err);
+    return;
+
+  }
+
+})
+
+// List all expenses
+router.get('/expenses', async (req, res, next) => {
+
+  try {
+
+    const expenses = await Expenses.find();
+
+    res.status(200).json({
+      success: true,
+      results: expenses
+    });
+
+  } catch (err) {
+
+    next(err);
+    return;
+
+  }
+
+})
+
+module.exports = router;
