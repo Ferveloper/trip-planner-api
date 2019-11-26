596a35d26cefb101ed1f8ac9ceea14311bbe17bc
Ferveloper <fermervil@gmail.com>
2019-11-26 22:35:47 +0100

    Add SwaggerUI API docs

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./tripPlanner.json');
 
 const db = require('./lib/connectMongoose');
 const indexRouter = require('./routes/index');
@@ -19,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/', indexRouter);
 app.use('/v1', apiRouter);
